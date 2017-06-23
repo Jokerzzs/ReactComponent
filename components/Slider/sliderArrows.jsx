c6c568315eda89a5fbbd2feaa4b4fa7ceca251c3
Jokerzzs <490619376@qq.com>
2017-06-23 10:15:29 +0800

    Create sliderArrows.jsx

---
+++ components/Slider/sliderArrows.jsx
@@ -0,0 +1,19 @@
+export default class SliderArrows extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    handleArrowClick(option) {
+        this.props.turn(option);
+    }
+
+    render() {
+        return (
+            <div className="acs-slider-arrow-wrap">
+                <span className="acs-slider-arrow-left" onClick={this.handleArrowClick.bind(this, -1)}> </span>
+                {this.props.children ? this.props.children : ''}
+                <span className="acs-slider-arrow-right" onClick={this.handleArrowClick.bind(this, 1)}></span>
+            </div>
+        );
+    }
+}
