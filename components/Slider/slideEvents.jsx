cef074998ce8926a325fd70d537b016678cebafc
Jokerzzs <490619376@qq.com>
2017-06-23 10:16:43 +0800

    Create slideEvents.jsx

---
+++ components/Slider/slideEvents.jsx
@@ -0,0 +1,48 @@
+// const React = require('react');
+// const ReactDom = require('react-dom');
+import { render } from 'react-dom';
+import Event from "../widget/event.jsx";
+
+class Events extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        let Events = [];
+        this.props.NewInformation.map((item, ind) => {
+            Events.push(<Event key={'Event' + ind} title={item.title} month={item.month} day={item.day} href={item.href} time={item.time} location={item.location} />);
+        });
+        return (
+            <div>{Events}</div>
+        );
+    }
+}
+export default class SliderEvents extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        let {inputDate, left, speed, nowLocal } = this.props;
+        let count = inputDate.length;
+        let width = 100 / count + '%';
+
+        let SliderEvents = [];
+        inputDate.map((item, ind) => {
+            SliderEvents.push(
+                <li key={'Events' + ind} className="acs-slider-pic" style={{ width: width }} >
+                    <Events NewInformation={item} />
+                </li>
+            )
+        });
+
+        return (
+            <ul style={{
+                left: -100 * nowLocal + "%",
+                transitionDuration: speed + "ms",
+                width: count * 100 + "%"
+            }}>
+                {SliderEvents}
+            </ul>
+        );
+    }
+}
